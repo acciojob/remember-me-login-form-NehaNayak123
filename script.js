0b39afeefcbf32ec17517ccfcd4f5d7ebe037268
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 17:14:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", ()=>{
+	let userName=document.getElementById("username");
+	let password=document.getElementById("password");
+	let checkbox=document.getElementById("checkbox");
+	let submit=document.getElementById("submit");
+	let existing=document.getElementById("existing");
+
+	let savedName=localStorage.getItem("username")
+	let savedPassword=localStorage.getItem("password")
+	if(savedName && savedPassword){
+		existing.classList.remove("hidden")
+	}
+
+	submit.addEventListener("click", (e)=>{
+		e.preventDefault();
+		alert("Logged in as "+userName.value);
+		if(checkbox.checked){
+			localStorage.setItem("username", userName.value);
+			localStorage.setItem("password", password.value);
+		}
+		else{
+			localStorage.removeItem("username", userName.value);
+			localStorage.removeItem("password", password.value);
+			
+		}
+	})
+	existing.addEventListener("click", ()=>{
+		alert("Logged in as "+localStorage.getItem("username"))
+	})
+	
+})
+
+
+
+
+
+
+
